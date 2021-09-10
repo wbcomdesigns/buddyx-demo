c41e383dec631c72babcc4610024813812215ef5
Nitin Patil <pallavi@wbcomdesigns.com>
2021-09-10 15:26:14 +0530

    Added wbcom essential as recommended plugin

--- buddyx-demo-importer-config.php
+++ buddyx-demo-importer-config.php
@@ -117,7 +117,7 @@ function bdi_import_files() {
 			'import_preview_image_url'   	=> 'https://www.example.com/merlin/preview_import_image1.jpg',
 			'import_notice'              	=> __( 'A special note for this import.', 'buddyx-demo-Importer' ),
 			'preview_url'                	=> 'https://www.example.com/my-demo-1',
-			'import_plugins'               => array('elementor', 'classic-widgets', 'kirki', 'buddypress', 'woocommerce'),
+			'import_plugins'               => array( 'elementor', 'classic-widgets', 'kirki', 'buddypress', 'woocommerce', 'wbcom-essential' ),
 		),
 		array(
 			'import_file_name'           	=> 'BuddyX with BB Platform',
@@ -130,7 +130,7 @@ function bdi_import_files() {
 			'import_preview_image_url'   	=> 'https://www.example.com/merlin/preview_import_image1.jpg',
 			'import_notice'              	=> __( 'A special note for this import.', 'buddyx-demo-Importer' ),
 			'preview_url'                	=> 'https://www.example.com/my-demo-1',
-			'import_plugins'               => array('elementor', 'classic-widgets', 'kirki', 'buddyboss-platform', 'woocommerce'),
+			'import_plugins'               => array( 'elementor', 'classic-widgets', 'kirki', 'buddyboss-platform', 'woocommerce', 'wbcom-essential' ),
 		),
 
 	);
