d619902bc02a10e987036bde037a432bfc1f62cd
Nitin Patil <pallavi@wbcomdesigns.com>
2024-10-28 17:49:47 +0530

    Add suggestion notice for permalink structure update
    https://3.basecamp.com/5798509/buckets/37499979/card_tables/cards/7930250893

--- includes/one-click-demo-import/views/import.php
+++ includes/one-click-demo-import/views/import.php
@@ -92,6 +92,20 @@ $theme            = wp_get_theme();
 										?>
 									</p>
 								</div>
+								<div class="ocdi-content-notice ocdi-content-notice--warning">
+									<p>
+										<?php
+											printf(
+												esc_html__(
+													'Suggestion: Update the permalink structure after the demo import is completed successfully.',
+													'one-click-demo-import'
+												),
+												'<span class="ocdi-recommended-star"><img src="' . esc_url( OCDI_URL . 'assets/images/icons/star.svg' ) . '" alt="' . esc_attr__( 'Star icon', 'one-click-demo-import' ) . '"></span>',
+												$theme->name
+											);
+										?>
+									</p>
+								</div>
 							<?php endif; ?>
 						</div>
 						<div class="ocdi-install-plugins-content-footer">
