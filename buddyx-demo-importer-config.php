88a526f12d0b06e6ddbf161cdfdf8e979f9a3e86
Varun Kumar Dubey <vapvarun@gmail.com>
2026-03-06 15:54:56 +0530

    Update plugin download URLs: wbcom-essential 4.2.1, buddyboss-platform 2.20.0

--- buddyx-demo-importer-config.php
+++ buddyx-demo-importer-config.php
@@ -269,7 +269,7 @@ function bdi_ocdi_register_plugins( $plugins ) {
 			$theme_plugins[] = array(
 				'name'     => 'Wbcom Essential',
 				'slug'     => 'wbcom-essential',
-				'source'   => 'https://demos.wbcomdesigns.com/exporter/plugins/wbcom-essential/4.0.0/wbcom-essential.zip',
+				'source'   => 'https://demos.wbcomdesigns.com/exporter/plugins/wbcom-essential/4.2.1/wbcom-essential-4.2.1.zip',
 				'required' => true,
 			);
 		}
@@ -279,7 +279,7 @@ function bdi_ocdi_register_plugins( $plugins ) {
 			$theme_plugins[] = array(
 				'name'     => 'BuddyBoss Platform',
 				'slug'     => 'buddyboss-platform',
-				'source'   => 'https://buddyboss.com/free-downloads/buddyboss-platform.php',
+				'source'   => 'https://github.com/buddyboss/buddyboss-platform/releases/download/2.20.0/buddyboss-platform-2.20.0.zip',
 				'required' => true,
 			);
 		}
